00873ccfe42905f480eba5fd78638f545bb85268
Tom ANDRE <tom.andre.dev@outlook.com>
2023-03-09 17:02:53 +0100

    #11 Add auto prefixer

--- index.php
+++ index.php
@@ -13,7 +13,8 @@ if (isset($_POST['texts'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
+    <link rel="stylesheet" href="css/prefixed/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400&display=swap" rel="stylesheet">
